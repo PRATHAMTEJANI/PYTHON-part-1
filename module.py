0e9c36a803a40fb5497037ab5f60beae6603e210
PRATHAMTEJANI <prathamtejani4@gmail.com>
2025-07-12 18:20:54 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,24 @@
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="myusername",
+#   password="mypassword"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE mydatabase")
+
+##If this page is executed with no error, you have successfully created a database.
+
+def function():
+    pass
+
+function()
+for x in range(5):
+  pass
+print(x)
+
+for char in "python":
+  print(char) 
